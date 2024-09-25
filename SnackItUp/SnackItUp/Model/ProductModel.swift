5cad0b934ec86e8e73730a30c5062c65fcb24a70
Sumit Rodrigues <65523336+SumitRodrigues@users.noreply.github.com>
2024-09-25 14:47:47 -0700

    Added Collections

--- SnackItUp/SnackItUp/Model/ProductModel.swift
+++ SnackItUp/SnackItUp/Model/ProductModel.swift
@@ -13,18 +13,19 @@ struct Product: Identifiable {
     var name: String
     var category: String
     var image: String
+    var color: Color
     var price: Int
 }
 
 // Sample Products
 
 var productList = [
-    Product(name: "", category: "", image: "", price: 8),
-    Product(name: "", category: "", image: "", price: 8),
-    Product(name: "", category: "", image: "", price: 8),
-    Product(name: "", category: "", image: "", price: 8),
-    Product(name: "", category: "", image: "", price: 8),
-    Product(name: "", category: "", image: "", price: 8),
-    Product(name: "", category: "", image: "", price: 8),
-    Product(name: "", category: "", image: "", price: 8),
+    Product(name: "Good Source", category: "choco", image: "image 1",color: .pink, price: 8),
+    Product(name: "Unreal Muffins", category: "choco", image: "image 2",color: .yellow, price: 8),
+    Product(name: "Twister Chips", category: "chips", image: "image 3",color: .green, price: 8),
+    Product(name: "Coconus Chips", category: "chips", image: "image 4",color: .red, price: 8),
+    Product(name: "Regular Nature", category: "chips", image: "image 5",color: .blue, price: 8),
+    Product(name: "Dark Russet", category: "chips", image: "image 6",color: .brown, price: 8),
+    Product(name: "Lion Chips", category: "chips", image: "image 7",color: .orange, price: 8),
+    Product(name: "Steep River", category: "chips", image: "image 8",color: .purple, price: 8),
 ]
--- SnackItUp/SnackItUp/View/Home.swift
+++ SnackItUp/SnackItUp/View/Home.swift
@@ -37,6 +37,15 @@ struct Home: View {
                 }
                 .padding(.horizontal ,30)
                 .padding(.vertical, 15)
+                
+                // Product List
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(productList, id: \.id) {
+                            item in ProductCard(product: item)
+                        }
+                    }
+                }
             }
             
         }
@@ -73,4 +82,54 @@ struct Home: View {
 
 #Preview {
     Home()
+}
+
+// Product Card View
+struct ProductCard: View {
+    var product: Product
+    
+    var body: some View {
+        ZStack {
+            VStack(alignment: .leading, content: {
+                Text("\(product.name)").font(.system(size: 30, weight: .semibold))
+                
+                Text(product.category)
+                    .font(.callout)
+                    .padding()
+                    .background(.white.opacity(0.5))
+                    .clipShape(Capsule())
+                
+                Spacer()
+                
+                HStack{
+                    Text("$\(product.price).0")
+                        .font(.system(size: 24, weight: .semibold))
+                    
+                    Spacer()
+                    
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "basket")
+                            .imageScale(.large)
+                            .frame(width: 90, height: 68)
+                            .background(.black)
+                            .clipShape(Capsule())
+                            .foregroundColor(.white)
+                    }
+                }
+                .padding(.leading)
+                .padding()
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                .frame(height: 80)
+                .background(.white.opacity(0.5))
+                .clipShape(Capsule())
+            })
+        }
+        .padding(30)
+        .frame(width: 336, height: 422)
+        .background(product.color.opacity(0.2))
+        .clipShape(.rect(cornerRadius: 57))
+        .padding(.leading, 20)
+    }
 }
